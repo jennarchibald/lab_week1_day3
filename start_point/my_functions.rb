cbc99b270725cce264786856e7e23ec6ba676799
jennarchibald <codeclan@CodeClans-MacBook-Pro.local>
2019-02-27 14:48:49 +0000

    solving second problem

--- start_point/my_functions.rb
+++ start_point/my_functions.rb
@@ -6,3 +6,15 @@ def add_array_lengths(array1, array2)
   end
   return array_lengths
 end
+
+
+def sum_array(numbers)
+
+  sum = 0
+
+  for number in numbers
+    sum += number
+  end
+  return sum
+
+end
